a78a8db4a947a83fc1007f712c02476aadfd5b10
6v6DS <dasul100@ajou.ac.kr>
2025-05-13 13:43:44 +0900

    fix: s3 경로 fix

--- src/main/java/com/uplus/ureka/controller/S3Controller.java
+++ src/main/java/com/uplus/ureka/controller/S3Controller.java
@@ -1,30 +1,30 @@
-package com.uplus.ureka.controller;
-
-import com.uplus.ureka.service.s3.S3Service; // ← S3Service 위치에 맞게 수정
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import org.springframework.web.multipart.MultipartFile;
-
-import java.util.List;
-
-@RestController
-@RequiredArgsConstructor
-@RequestMapping("/file")
-public class S3Controller {
-
-    private final S3Service s3Service;
-
-    @PostMapping
-    public ResponseEntity<List<String>> uploadFile(@RequestParam("files") List<MultipartFile> multipartFiles){
-        return ResponseEntity.ok(s3Service.uploadFile(multipartFiles));
-    }
-
-    @DeleteMapping
-    public ResponseEntity<String> deleteFile(@RequestParam String fileName){
-        s3Service.deleteFile(fileName);
-        return ResponseEntity.ok(fileName);
-    }
-}
+//package com.uplus.ureka.controller;
+//
+//import com.uplus.ureka.service.s3.S3Service; // ← S3Service 위치에 맞게 수정
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//import org.springframework.web.multipart.MultipartFile;
+//
+//import java.util.List;
+//
+//@RestController
+//@RequiredArgsConstructor
+//@RequestMapping("/file")
+//public class S3Controller {
+//
+//    private final S3Service s3Service;
+//
+//    @PostMapping
+//    public ResponseEntity<List<String>> uploadFile(@RequestParam("files") List<MultipartFile> multipartFiles){
+//        return ResponseEntity.ok(s3Service.uploadFile(multipartFiles));
+//    }
+//
+//    @DeleteMapping
+//    public ResponseEntity<String> deleteFile(@RequestParam String fileName){
+//        s3Service.deleteFile(fileName);
+//        return ResponseEntity.ok(fileName);
+//    }
+//}
